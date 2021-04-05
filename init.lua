cca7e3b3d8f99955fea1aad5d16f9c6575985504
Von Welch <von@vwelch.com>
2021-04-05 13:01:44 -0400

    _applyScreen():Adding debug logging for window:frame():type

--- init.lua
+++ init.lua
@@ -565,6 +565,8 @@ end
 -- * Nothing
 function Contexts:_applyScreen(screen, window)
   self.log.df("Moving window \"%s\" to screen %s", window:title(), screen:name())
+  -- DEBUG
+  self.log.df("DEBUG: window:frame():type == %s", screen:frame():type())
   -- keep relative size, keep in bounds
   window:moveToScreen(screen, false, true)
 end
